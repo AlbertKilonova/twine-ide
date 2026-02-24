49eaf5ffdf0420e345507e4c48de11f3a6ad6f67
AlbertKilonova <Albert.Kilonova@gmail.com>
2026-02-24 22:18:04 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
