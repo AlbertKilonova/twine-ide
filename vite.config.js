1f558dc97c5d68402f5a93213f9d9dfd615bf569
AlbertKilonova <Albert.Kilonova@gmail.com>
2026-02-25 10:22:47 +0800

    style(index.html pwa): 添加图标

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,59 @@
-import { defineConfig } from 'vite'
+import {
+    defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import {
+    VitePWA
+} from 'vite-plugin-pwa'
 import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    chunkSizeWarningLimit: 1500,
-  },
-  plugins: [
-  vue(),
-  VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-          name: "Apo's TwineIDE",
-          short_name: "TwineIDE",
-          description: "一个用于Twee的编辑器",
-          start_url: "./index.html",
-          display: "standalone",
-          background_color: "#1e1e1e",
-          theme_color: "#007acc",
-          icons: [
-            {
-              src: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/72x72/2728.png",
-              sizes: "72x72",
-              type: "image/png",
-              purpose: 'any maskable'
+    base: './',
+    build: {
+        chunkSizeWarningLimit: 1500,
+    },
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg', 'pwa.png', 'pwa-maskable.png'],
+            manifest: {
+                name: "Apo's TwineIDE",
+                short_name: "TwineIDE",
+                description: "一个用于Twee的编辑器",
+                start_url: "./index.html",
+                display: "standalone",
+                background_color: "#1e1e1e",
+                theme_color: "#007acc",
+                icons: [{
+                        src: 'pwa.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-maskable.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    },
+                    {
+                        src: 'favicon.svg',
+                        sizes: 'any',
+                        type: 'image/svg+xml',
+                        purpose: 'any'
+                    }
+                ]
             },
-            {
-              src: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/512x512/2728.png",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: 'any maskable'
+            workbox: {
+                cacheId: `twine-ide-${pkg.version}`,
+                globPatterns: ['**/*.{js,css,html,png,svg,ico}']
             }
-          ]
-        },
-      workbox: {
-        cacheId: `twine-ide-${pkg.version}`,
-        globPatterns: ['**/*.{js,css,html,png,svg,ico}']
-      }
-    })
+        })
     ],
 })
